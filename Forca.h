4edef5a663d7d91db7189d1e29e2c5d6b924b2a2
Lucas Melo <64665653+meloluvert@users.noreply.github.com>
2023-03-11 08:40:40 -0300

    Update Forca.h
    Resolvendo erros

--- Forca.h
+++ Forca.h
@@ -1,22 +1,10 @@
 #include <iostream>
 class Forca
 {
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> origin/master
-
-
     public:
     void mostra(std::string, std::string, int);
         const int TAM = 89;
         /*essa classe string está dentro do espaço de nomes padrão*/
-<<<<<<< HEAD
-        
-=======
-
->>>>>>> origin/master
         std::string palavras[89] = {"sogra", "abacaxi", "banana", "melancia", "cachorro", "gato", "piano", "framboesa", "amora", "acerola", "caju", "frango", "galinha", "morango", "cereja", "kiwi", "porta", "tecnologia", "tatu", "resumo", "pipoca", "carambola", "abacate", "macaco", "laranja", "manga", "morcego", "papagaio", "jaca", "goiaba", "uva", "chocolate", "queijo", "bolo", "pipa", "casa", "mesa", "cadeira", "carro", "bicicleta", "barco", "navio", "peixe", "marisco", "lagosta", "caranguejo", "sushi", "pizza", "hamburguer", "arroz", "batata", "cenoura", "abobrinha", "berinjela", "couve", "alface", "tomate", "cebola", "alho", "salsa", "coentro", "chuchu", "vagem", "milho", "amendoim", "castanha", "noz", "pistache", "figo", "damasco", "nectarina", "margarida", "girassol", "rosa", "cravo", "lírio", "tulipa", "violeta", "cacto", "bambu", "pinheiro", "cedro", "jacarandá", "mangueira", "jambo", "cerejeira", "oliveira", "abacateiro", "jabuticabeira"};
         std::string Sortear();
         //só o tipo
